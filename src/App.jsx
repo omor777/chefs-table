6f48f1ba320f75bd3ae0a8ea65d010dd1f89d4b9
omor777 <omor0843@gmail.com>
2024-03-16 19:40:23 +0600

    responsive cart section

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 function App() {
-   const [toggleMenu, setToggleMenu] = useState(false);
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-   const handleToggleMenu = () => {
-      setToggleMenu((prev)=>!prev)
-   }
+  const handleToggleMenu = () => {
+    setToggleMenu((prev) => !prev);
+  };
   return (
     <>
       <Header isMenuOpen={toggleMenu} handleToggleMenu={handleToggleMenu} />
--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -9,42 +9,45 @@ function Cart({ recipes, handleAddToCooking, cookingRecipe }) {
           Want to cook: {recipes.length}
         </h4>
         <div className="divider w-2/3 mx-auto"></div>
-        <table className="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th>Name</th>
-              <th>Time</th>
-              <th>Calories</th>
-            </tr>
-          </thead>
-          {recipes.map((recipe, index) => (
-            <tbody key={recipe.id}>
-              <tr className="bg-[#28282808]">
-                <td>{index + 1}</td>
-                <td>{recipe.recipe_name}</td>
-                <td>{recipe.preparing_time} minutes</td>
-                <td>{recipe.calories} calories</td>
-                <td>
-                  <button
-                    onClick={() => {
-                      handleAddToCooking(recipe);
-                    }}
-                    className="bg-main px-[18px] py-[9px] rounded-full font-medium text-black-900"
-                  >
-                    Preparing
-                  </button>
-                </td>
+        <div className="overflow-x-auto">
+          <table className="table-sm lg:table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Time</th>
+                <th>Calories</th>
+                <th></th>
               </tr>
-            </tbody>
-          ))}
-        </table>
+            </thead>
+            {recipes.map((recipe, index) => (
+              <tbody key={recipe.id}>
+                <tr className="bg-[#28282808] ">
+                  <td>{index + 1}</td>
+                  <td>{recipe.recipe_name}</td>
+                  <td>{recipe.preparing_time} minutes</td>
+                  <td>{recipe.calories} calories</td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        handleAddToCooking(recipe);
+                      }}
+                      className="bg-main py-2 px-4 lg:px-[18px] lg:py-[9px] rounded-full text-sm lg:text-base font-medium text-black-900"
+                    >
+                      Preparing
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
         {/* two table */}
         <h4 className="text-2xl font-semibold text-black-900 text-center mt-8">
           Currently cooking: {cookingRecipe.length}
         </h4>
         <div className="divider w-2/3 mx-auto"></div>
-        <table className="table">
+        <table className="table-sm  lg:table w-full">
           <thead>
             <tr>
               <th></th>
@@ -67,20 +70,21 @@ function Cart({ recipes, handleAddToCooking, cookingRecipe }) {
           })}
         </table>
         <div className="flex items-center justify-center gap-8 mt-6">
-          <p className="text-black-800 font-medium leading-7 flex flex-col">
-            Total Time{" "}
+          <p className="text-black-800 font-medium leading-7 flex flex-col items-center text-center">
+            Total Time
             <span>
               {cookingRecipe.reduce(
                 (acc, curr) => acc + curr.preparing_time,
                 0
-              )}{" "}
+              )}
+              {"  "}
               minute
             </span>
           </p>
-          <p className="text-black-800 font-medium leading-7 flex flex-col">
-            Total Calories{" "}
+          <p className="text-black-800 font-medium leading-7 flex flex-col text-center">
+            Total Calories
             <span>
-              {cookingRecipe.reduce((acc, curr) => acc + curr.calories, 0)}{" "}
+              {cookingRecipe.reduce((acc, curr) => acc + curr.calories, 0)}
               calories
             </span>
           </p>
